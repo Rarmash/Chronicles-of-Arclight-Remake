ccd4a43b0e9da1569774898a0edc4a11331e6cf7
Andrew <62994051+Rarmash@users.noreply.github.com>
2026-09-01 01:03:51 +0300

    Add character creation flow

--- Sources/ChroniclesCLI/main.swift
+++ Sources/ChroniclesCLI/main.swift
@@ -1,12 +1,40 @@
 import ArclightCore
 import ChroniclesGame
 
-let player = ChroniclesPlayer(
-    name: "Andrew",
-    characterClass: .warrior
-)
+print("Chronicles of Arclight")
+print()
+print("Enter your name:")
+if let name = readLine() {
+    print()
+    print("Choose your class:")
+    print("1. Heavy")
+    print("2. Medic")
+    print("3. Warrior")
+    if let input = readLine(),
+        let number = Int(input) {
+            var selectedClass: CharacterClass? = nil
 
-print(player.name)
-print(player.characterClass)
-print(player.stats.health)
-print(player.stats.power)
+            switch number {
+                case 1:
+                    selectedClass = .heavy
+                case 2:
+                    selectedClass = .medic
+                case 3:
+                    selectedClass = .warrior
+                default:
+                    print("Invalid class.")
+            }
+
+            if let characterClass = selectedClass {
+                let player = ChroniclesPlayer(
+                    name: name,
+                    characterClass: characterClass
+                )
+
+                print("Name: \(player.name)")
+                print("Class: \(player.characterClass)")
+                print("HP: \(player.stats.health)/\(player.stats.maxHealth)")
+                print("Power: \(player.stats.power)")
+            }
+        }
+}
